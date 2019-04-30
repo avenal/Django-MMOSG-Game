83c40016fd63b76e8d88c34437f8faeeb43ce691
avenal <avenalszarosiwy@gmail.com>
2019-04-30 12:31:19 +0200

    basic project setup

--- MMOSG/settings.py
+++ MMOSG/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'game',
+    'accounts',
     'django_q',
 ]
 
@@ -70,7 +71,7 @@ ROOT_URLCONF = 'MMOSG.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -133,4 +134,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'MMOSG/static')
+]
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
